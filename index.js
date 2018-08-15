21c37c438e8e801452f813f75e3d3ea9909c83f4
Merna khaled <merna.khaled.5049@gmail.com>
2018-08-15 13:53:12 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,27 @@
+function takeANumber(num, name) {
+  num++
+  return "Welcome, " + name + ". You are number " + num + " in line."
+}
+
+function nowServing(line) {
+  if(line.length > 0) {
+    return line.shift()
+  }
+  else {
+    return "There is nobody waiting to be served!"
+  }
+}
+
+function currentLine(line) {
+  if(line.length > 0) {
+    var s = "The line is currently:"
+    for(var i = 0; i < line.length; i++) {
+      s += " " + (i + 1) + ". " + line[i] + ","
+    }
+    s.pop()
+    return s
+  }
+  else {
+    return "The line is currently empty."
+  }
+}
