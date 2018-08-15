65643b73d9cc219e8cc278897660b2c6dd288c13
Merna khaled <merna.khaled.5049@gmail.com>
2018-08-15 14:10:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,8 +18,7 @@ function currentLine(line) {
     for(var i = 0; i < line.length; i++) {
       s += " " + (i + 1) + ". " + line[i] + ","
     }
-    s.slice(s.length - 2)
-    return s
+    return s.slice(s.length - 1)
   }
   else {
     return "The line is currently empty."
